a08fa4d49361fed35d8ba9becdf18fcc42f0636f
MishaNikasov <mishanikasov@mail.ru>
2022-07-25 10:27:08 +0300

    * refactoring: move nav host to router

--- app/src/main/java/com/app/noteapp/navigation/Router.kt
+++ app/src/main/java/com/app/noteapp/navigation/Router.kt
@@ -1,10 +1,36 @@
 package com.app.noteapp.navigation
 
-import androidx.navigation.NavController
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.app.noteapp.view.screen.home.HomeScreen
+import com.app.noteapp.view.screen.note.NoteScreen
 
 class Router(
-    private val navController: NavController
+    private val navController: NavHostController
 ) {
+
+    @Composable
+    fun NoteAppNavHost() {
+        NavHost(navController = navController, startDestination = Screen.Home.route ) {
+            composable(Screen.Home.routeWithArgs) { HomeScreen(router = this@Router, homeViewModel = hiltViewModel()) }
+            composable(
+                route = Screen.Note.routeWithArgs,
+                arguments = listOf(navArgument("noteId") {
+                    nullable = true
+                    defaultValue = null
+                    type = NavType.StringType }
+                )
+            ) { backStackEntry ->
+                val noteId = backStackEntry.arguments?.getString("noteId")?.toLongOrNull()
+                NoteScreen(router = this@Router, noteViewModel = hiltViewModel(), noteId)
+            }
+        }
+    }
 
     fun openNote(noteId: Long?) {
         navController.navigate(Screen.Note.putArgs(noteId.toString()))
--- app/src/main/java/com/app/noteapp/view/MainActivity.kt
+++ app/src/main/java/com/app/noteapp/view/MainActivity.kt
@@ -3,16 +3,8 @@ package com.app.noteapp.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.app.noteapp.navigation.Router
-import com.app.noteapp.navigation.Screen
-import com.app.noteapp.view.screen.home.HomeScreen
-import com.app.noteapp.view.screen.note.NoteScreen
 import com.app.presentation.theme.NoteAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,20 +17,7 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             val router = Router(navController)
             NoteAppTheme {
-                NavHost(navController = navController, startDestination = Screen.Home.route ) {
-                    composable(Screen.Home.routeWithArgs) { HomeScreen(router = router, homeViewModel = hiltViewModel()) }
-                    composable(
-                        route = Screen.Note.routeWithArgs,
-                        arguments = listOf(navArgument("noteId") {
-                            nullable = true
-                            defaultValue = null
-                            type = NavType.StringType }
-                        )
-                    ) { backStackEntry ->
-                        val noteId = backStackEntry.arguments?.getString("noteId")?.toLongOrNull()
-                        NoteScreen(router = router, noteViewModel = hiltViewModel(), noteId)
-                    }
-                }
+                router.NoteAppNavHost()
             }
         }
     }
