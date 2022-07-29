e2fb183faec505945be9aca17551fbc2ee28fa8f
MishaNikasov <mishanikasov@mail.ru>
2022-07-30 00:40:56 +0300

    * fixed: start destination for authorized user

--- app/src/main/java/com/app/noteapp/view/screen/splash/AppState.kt
+++ app/src/main/java/com/app/noteapp/view/screen/splash/AppState.kt
@@ -4,5 +4,5 @@ import com.app.noteapp.navigation.Screen
 
 sealed class AppState(val screen: Screen) {
     object Unauthorized: AppState(Screen.Auth)
-    object Authorized: AppState(Screen.Auth)
+    object Authorized: AppState(Screen.Home)
 }
